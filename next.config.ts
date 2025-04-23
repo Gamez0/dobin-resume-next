750e8e9f2797c070a545359c0c6488ef829984a8
Dobin Shin <34051876+Gamez0@users.noreply.github.com>
2025-04-23 06:18:46 +0000

    c: gh page deploy setting

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     /* config options here */
+    output: 'export',
+    basePath: '/dobin-resume-2.0/',
+    trailingSlash: true,
 };
 
 export default nextConfig;
