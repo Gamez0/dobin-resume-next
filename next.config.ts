9f9e801574f64896f7ed7c55aef2333b151688fe
Dobin Shin <34051876+Gamez0@users.noreply.github.com>
2025-04-23 07:24:10 +0000

    c: asset prefix

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
+const isProd = process.env.NODE_ENV ==='production';
+
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     /* config options here */
-    output: 'export',
-    basePath: '/dobin-resume-next',
+    output: isProd ? 'export' : 'standalone',
+    basePath: isProd ? '/dobin-resume-next' : '',
+    assetPrefix: isProd ? '/dobin-resume-next/' : '',
     trailingSlash: true,
 };
 
